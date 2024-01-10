3fbd9c04977c82247ceb0290ec81896f431aff67
Just0jo <114908443+Just0jo@users.noreply.github.com>
2024-01-10 22:55:27 +0000

    commit

--- src/main/java/ie/atu/Project_1/UserController.java
+++ src/main/java/ie/atu/Project_1/UserController.java
@@ -1,30 +1,20 @@
-package ie.atu.Project_1;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class UserController {
-    private CatalogServiceClient catalogServiceClient;
+
     private final UserService userService;
 
     @Autowired
-    public UserController(UserService userService, CatalogServiceClient catalogServiceClient) {
-
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.catalogServiceClient = catalogServiceClient;
     }
 
-    @PostMapping("/details")
-    public String details(@RequestBody User user) {
-        String confirm = catalogServiceClient.someDetails(user);
-        String response = confirm +" "+  userService.ackMessage(user);
-
-        return response;
-
+    @PostMapping("/catalog")
+    public String user(@RequestBody User user) {
+        return userService.ackMessage(user);
     }
-
 }
